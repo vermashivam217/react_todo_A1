83ff3125f7f32dfa03b8350ba86d6d20f8e9f96b
Shivam Soni <vermashivam217@gmail.com>
2021-07-21 14:10:27 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Todo from './component/todo';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      < Todo />
+    </>
+
+  );
+}
+
+export default App;
